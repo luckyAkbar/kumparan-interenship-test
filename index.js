9e4ef85d04903d9f3cf3f786d1267b99908da84f
luckyAkbar <luckyakbar1509@gmail.com>
2022-01-12 20:50:58 +0700

    change server listening port from hardcoded to softcoded

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+require('dotenv').config();
+
 const app = require('./src/app');
 const db = require('./connection/dbConnect');
 
-app.listen(3000, async () => {
+app.listen(Number(process.env.PORT), async () => {
   try {
     await db.connect();
     console.log('db connect. listening on 3000');
